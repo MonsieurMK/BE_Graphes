6ab718747eaf8c00f83af193050fd360a638c5fa
MonsieurSinge <momopelloux@gmail.com>
2021-04-12 12:17:23 +0200

    Dijkstra en cours (changer et optimiser condition, implementer remplissage resultat)

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,14 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractSolution;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Label;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
+import java.util.ArrayList;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +18,88 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
         // TODO:
+
+        BinaryHeap tas = new BinaryHeap();
+
+        // Initialisation
+        ArrayList<Label> labels = new ArrayList<>();
+        Label label;
+        Label labelDest = null;
+
+        for (Node node : this.getInputData().getGraph().getNodes()) {
+            if (node.equals(this.getInputData().getOrigin())) {
+                label = new Label(node, false, 0, null);
+                tas.insert(label);
+            } else if (node.equals(this.getInputData().getDestination())) {
+                labelDest = new Label(node, false, 0, null);
+                label = labelDest;
+            } else {
+                label = new Label(node, false, Double.POSITIVE_INFINITY, null);
+            }
+            labels.add(label);
+        }
+
+        // Iterations
+        ArrayList<Arc> arcs = new ArrayList<>();
+        Arc succ_dest = null;
+        boolean fin = false;
+        Node x;
+        label = (Label) tas.findMin();
+        x = label.getSommetCourant();
+        while (!x.equals(this.getInputData().getDestination())) {
+            label = (Label) tas.deleteMin();
+            label.setMarque(true);
+            x = label.getSommetCourant();
+            Label labY;
+            for (Arc successeur : x.getSuccessors()) {
+                for (Label label_successeur : labels) {
+                    if (label_successeur.getSommetCourant().equals(successeur.getDestination())) {
+                        labY = label_successeur;
+                        if (!labY.isMarque()) {
+                            if (labY.getCout() > label.getCout() + successeur.getLength()) {
+                                labY.setCout(label.getCout() + successeur.getLength());
+                                //tas.insert(labY);
+                                labY.setPere(successeur);
+                            }
+                        }
+                        arcs.add(successeur);
+                        if (x.equals(this.getInputData().getDestination())) {
+                            succ_dest = successeur;
+                        }
+                    }
+                }
+            }
+
+            // verif si tous sommets marqués
+            // TODO optimiser
+            fin = true;
+            for (Label label_courant : labels) {
+                if (!label_courant.isMarque()) {
+                    fin = false;
+                }
+            }
+        }
+        // Creation chemin
+        // TODO
+        ArrayList<Arc> arcs_solution = new ArrayList<>();
+        Arc successeur_courant = succ_dest;
+        Node node_courant = successeur_courant.getOrigin();
+        while (successeur_courant != null) {
+            arcs.add(successeur_courant);
+            for (Label label_courant : labels) {
+                if (label_courant.getPere().equals(successeur_courant)) {
+                    node_courant = label_courant.getSommetCourant();
+                }
+            }
+            for (Label label_courant : labels) {
+                if (label_courant.getPere().getDestination().equals(node_courant)) {
+                    successeur_courant = label_courant.getPere();
+                }
+            }
+        }
+
+        ShortestPathSolution solution = new ShortestPathSolution(data, AbstractSolution.Status.OPTIMAL, new Path(this.getInputData().getGraph(), arcs));
 
         return solution;
     }
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,12 +1,12 @@
 package org.insa.graphs.model;
 
-public class Label {
+public class Label implements Comparable {
     private Node sommetCourant;
     private boolean marque;
     private double cout;
-    private Node pere;
+    private Arc pere;
 
-    public Label(Node sommetCourant, boolean marque, double cout, Node pere) {
+    public Label(Node sommetCourant, boolean marque, double cout, Arc pere) {
         this.sommetCourant = sommetCourant;
         this.marque = marque;
         this.cout = cout;
@@ -17,7 +17,37 @@ public class Label {
         return cout;
     }
 
+    public Node getSommetCourant() {
+        return this.sommetCourant;
+    }
+
     public void setSommetCourant(Node sommetCourant) {
         this.sommetCourant = sommetCourant;
+    }
+
+    public boolean isMarque() {
+        return marque;
+    }
+
+    public void setMarque(boolean marque) {
+        this.marque = marque;
+    }
+
+    public void setCout(double cout) {
+        this.cout = cout;
+    }
+
+    public void setPere(Arc pere) {
+        this.pere = pere;
+    }
+
+    public Arc getPere() {
+        return this.pere;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO
+        return 0;
     }
 }
